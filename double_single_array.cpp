b255af4866122e4503519cd0f682bba4a08ad875
andrushechka365 <142117922+andrushechka365@users.noreply.github.com>
2023-09-04 23:57:03 +0300

    setter function added

--- double_single_array.cpp
+++ double_single_array.cpp
@@ -4,7 +4,17 @@
 int getter(int *text, int index_x, int index_y) {
     return *(text + 2 + index_y * *(text + 1) + index_x);
 }
+void setter(int *text, int x, int y, int target) {
+    *(text + y * *(text + 1) + x + 2) = target;
+}
 void print_hype_array(int *text) {
+    for (int y = 0; y < *text; y++, printf("\n")) {
+        for(int x = 0; x < *(text + 1); x++) {
+            printf("%d ", getter(text, x, y));
+        }
+    }
+    putchar('\n');
+    setter(text, 1, 1, 99);
     for (int y = 0; y < *text; y++, printf("\n")) {
         for(int x = 0; x < *(text + 1); x++) {
             printf("%d ", getter(text, x, y));
