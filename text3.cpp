bf5f3fd6cfcbb28388e700fc6bcd7d57cd44db77
andrushechka365 <142117922+andrushechka365@users.noreply.github.com>
2023-09-05 01:12:33 +0300

    realization of text4

---
+++ text3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main(void) {
+    char buffer[100] = {0};
+    int n, len_max;
+    for(int i = 1; i <= n; i++) {
+        gets(buffer);
+        int const len = strlen(buffer);
+
+
+    }
+    gets(buffer);
+    const int len = 10;
+    char *text[len] = {0};
+
+
+}
---
+++ text4.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+// stores the massive of pointers
+int main(void) {
+    char *text[] = {0};
+    int s = 0;
+    scanf("%d\n", &s);
+    int const len = s;
+    char buffer[100] = {0};
+    for (int i = 0; i < s; i++) {
+        fgets(buffer, 100, stdin);
+        char * pointer = (char *) calloc(strlen(buffer) + 1, 1);
+        strcpy(pointer, buffer);
+        text[i] = pointer;
+    }
+    for (int i = 0; i < s; i++) {
+        puts(text[i]);
+    }
+    for(int i = 0; i < s; i++)
+        free(text[i]);
+    return 0;
+
+}
